8bf5290a9f8e4019b7c2e35dd1807b7f6adfdfbc
John Doe <johndoe@example.com>
2023-08-23 15:07:35 +0200

    ouazoizzzz

--- NFT/app/src/contractInteractions.js
+++ NFT/app/src/contractInteractions.js
@@ -1,8 +1,10 @@
 import Web3 from 'web3';
-import contractABI from './contratMariageABI.json'; // Remplacez par le chemin vers le fichier ABI
-const contractAddress = '0x...'; // Adresse de votre contrat
+import contractABI from './ContratMariageABI.json';
+const contractAddress = '0xc817680A20028390E7E2C6c3e38eAF57FcA08834';
 
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); // Connexion à Ganache ou Sepolia
+// Utilisez l'URL correcte du fournisseur pour Ganache
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 export default contract;
+
